b88156684f1c53e63055eb6dc6b54324ea2c2b98
Tamirshazman <A0547799597t>
2020-12-15 20:44:58 +0200

    delete Table

---
+++ delete Table.sql
@@ -0,0 +1,21 @@
+
+Drop TABLE spectated;
+Drop TABLE Game;
+Drop TABLE teamOfYear;
+Drop TABLE plays;
+Drop TABLE MagicAct;
+Drop TABLE spell;
+Drop TABLE loyalty;
+Drop TABLE NonWizard;
+Drop TABLE attendsHomogeneousSchool;
+DROP TABLE schoolManager;
+DROP TABLE Wizard;
+DROP TABLE House;
+DROP TABLE headsHouse;
+DROP TABLE attendsHouse;
+DROP TABLE attendsSchool;
+DROP TABLE heterogeneousSchool;
+DROP TABLE homogeneousSchool;
+DROP TABLE School;
+DROP TABLE feeling;
+DROP TABLE Character;
