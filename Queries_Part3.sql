f8c87584fdd0c68d8a680faab2f8575e0bd84363
Tamirshazman <A0547799597t>
2020-12-15 18:35:14 +0200

    finsied with the querties

--- Queries_Part3.sql
+++ Queries_Part3.sql
@@ -12,9 +12,25 @@ SM.wName = MA.wName
 GROUP BY SM.managerName;
 HAVING COUNT (MA.time) > 0
 
-
-SELECT
-FROM Wizard
-WHERE NOT EXISTS ((SELECT * FROM feeling Where Wizard.wName = feeling.cNameOne) MINUS
-                  (SELECT * FROM feeling WHERE Wizard.wName = feeling.cNameOne AND
+CREATE VIEW doucheWizard_And_UsefulMagic
+as
+SELECT Wizard.wName, COUNT(MagicAct.time) AS NumberOfAct
+FROM Wizard, MagicAct
+WHERE Wizard.wName = MagicAct.wName AND
+    NOT EXISTS(
+        (SELECT * FROM feeling WHERE Wizard.wName = feeling.cNameOne) EXCEPT
+        (SELECT * FROM feeling WHERE Wizard.wName = feeling.cNameOne AND
                    feeling.feeling = 'LOVE')) AND
+    NOT EXISTS (
+        (SELECT * FROM feeling WHERE Wizard.wName = feeling.cNameTwo) EXCEPT
+        (SELECT * FROM feeling WHERE Wizard.wName = feeling.cNameTwo AND
+                   feeling.feeling = 'HATE')) AND
+    NOT EXISTS(
+        (SELECT DISTINCT CONVERT(date, MagicAct.time) FROM MagicAct) EXCEPT
+        (SELECT DISTINCT CONVERT(date, MagicAct.time) FROM MagicAct WHERE Wizard.wName = MagicAct.wName)
+        ) AND
+    EXISTS(SELECT * FROM feeling WHERE feeling.cNameOne = Wizard.wName AND feeling.feeling = 'LOVE') AND
+    EXISTS(SELECT * FROM feeling WHERE feeling.cNameTwo = Wizard.wName AND feeling.feeling = 'HATE')
+GROUP BY Wizard.wName
+ORDER BY COUNT(MagicAct.time) DESC;
+
